ac9195b635ed7a252e2bedc4bb5f6adc20cbfa5c
WillC33 <williamcookenet@pm.me>
2025-10-17 15:31:56 +0200

    feat(highlights): trials further semantic highlighting and enhanced comments

--- lua/heathglass/highlights.lua
+++ lua/heathglass/highlights.lua
@@ -1,5 +1,6 @@
 -- HeathGlass Highlight Definitions
 -- Semantic highlighting following warm/cool principle
+-- Refined to reduce grammatical noise whilst maintaining readability
 
 local M = {}
 
@@ -89,127 +90,140 @@ function M.get_highlights(colors, config)
 
   -- SEMANTIC SYNTAX HIGHLIGHTING
   -- Following melange principle: warm for control, cool for data
+  -- Refined to reduce grammatical noise
 
   -- WARM COLORS - Control flow and actions
 
-  -- Control structures (lilac tones) - guide logical flow
-  highlights.Keyword = style({ fg = colors.purple }, config.styles.keywords)
-  highlights.Conditional = style({ fg = colors.purple }, config.styles.keywords)
-  highlights.Repeat = style({ fg = colors.purple }, config.styles.keywords)
-  highlights.Exception = style({ fg = colors.purple }, config.styles.keywords)
-  highlights.Include = { fg = colors.purple }
-  highlights.StorageClass = { fg = colors.purple }
-  highlights.Structure = { fg = colors.purple }
-  highlights.Typedef = { fg = colors.purple }
+  -- Keywords - DE-EMPHASISED (using regular text colour with subtle styling)
+  -- Most keywords are just syntax, not semantically important
+  highlights.Keyword = style({ fg = colors.fg_dark }, config.styles.keywords)
 
-  -- Functions and methods (mauve tones) - active elements
+  -- Control flow - SLIGHTLY EMPHASISED (these DO matter semantically)
+  highlights.Conditional = { fg = colors.purple_dark } -- if/else/switch - dimmer purple
+  highlights.Repeat = { fg = colors.purple_dark }      -- for/while - dimmer purple
+  highlights.Exception = { fg = colors.purple }        -- try/catch - slightly brighter as these are important
+
+  -- Less important keywords - VERY DE-EMPHASISED
+  highlights.Include = { fg = colors.fg_dark }      -- import/include - just structure
+  highlights.StorageClass = { fg = colors.fg_dark } -- static/extern - not critical
+  highlights.Structure = { fg = colors.fg_dark }    -- struct/union
+  highlights.Typedef = { fg = colors.fg_dark }      -- typedef
+
+  -- Functions - EMPHASISED (these are the actions)
   highlights.Function = style({ fg = colors.magenta }, config.styles.functions)
-  highlights.Identifier = style({ fg = colors.magenta }, config.styles.functions)
-  highlights.Statement = { fg = colors.magenta }
+  highlights.Identifier = { fg = colors.fg }     -- DE-EMPHASISED - most identifiers are noise
+  highlights.Statement = { fg = colors.fg_dark } -- DE-EMPHASISED - statements are grammar
 
-  -- Variables and data being acted upon (peach tones)
+  -- Variables and data - WARM but distinct
   highlights.Variable = style({ fg = colors.orange }, config.styles.variables)
-  highlights.Constant = { fg = colors.orange_dark }
+  highlights.Constant = { fg = colors.orange_dark, bold = true } -- Constants more prominent
   highlights.Number = { fg = colors.orange_light }
   highlights.Float = { fg = colors.orange_light }
   highlights.Boolean = { fg = colors.orange_light }
 
   -- COOL COLORS - Data and structure
 
-  -- Strings and data content (sea-green tones)
+  -- Strings and data content - CLEAR but not dominant
   highlights.String = { fg = colors.green }
   highlights.Character = { fg = colors.green_light }
 
-  -- Types and structural definitions (teal tones)
+  -- Types - VISIBLE as they're semantically important
   highlights.Type = { fg = colors.cyan }
   highlights.TypeDef = { fg = colors.cyan }
 
-  -- Comments (muted tones) - passive information
-  highlights.Comment = style({ fg = colors.fg_gutter }, config.styles.comments)
-  highlights.SpecialComment = style({ fg = colors.fg_dark }, config.styles.comments)
+  -- Comments - MORE VISIBLE (bumped up from fg_gutter)
+  -- Comments are documentation, not noise!
+  highlights.Comment = style({ fg = colors.fg_dark, italic = true }, config.styles.comments)
+  highlights.SpecialComment = style({ fg = colors.fg, italic = true }, config.styles.comments)
   highlights.Todo = { fg = colors.yellow, bold = true }
   highlights.Note = { fg = colors.info, bold = true }
 
-  -- Operators and punctuation (neutral) - structural connectors
-  highlights.Operator = { fg = colors.fg_dark }
-  highlights.Delimiter = { fg = colors.fg_dark }
-  highlights.Punctuation = { fg = colors.fg_dark }
+  -- Operators and punctuation - INVISIBLE (pure grammar)
+  highlights.Operator = { fg = colors.fg }  -- Blend into normal text
+  highlights.Delimiter = { fg = colors.fg } -- Brackets, commas - just structure
+  highlights.Punctuation = { fg = colors.fg }
 
-  -- Special syntax (slate-blue) - meta-programming
-  highlights.Special = { fg = colors.blue }
+  -- Special syntax - DE-EMPHASISED
+  highlights.Special = { fg = colors.fg_dark }
   highlights.SpecialChar = { fg = colors.blue_light }
   highlights.Tag = { fg = colors.blue }
-  highlights.PreProc = { fg = colors.blue }
-  highlights.PreCondit = { fg = colors.blue }
-  highlights.Define = { fg = colors.blue }
-  highlights.Macro = { fg = colors.blue_light }
-  highlights.Label = { fg = colors.blue }
+  highlights.PreProc = { fg = colors.fg_dark } -- Preprocessor is mostly noise
+  highlights.PreCondit = { fg = colors.fg_dark }
+  highlights.Define = { fg = colors.fg_dark }
+  highlights.Macro = { fg = colors.blue_light }  -- Macros slightly visible as they hide complexity
+  highlights.Label = { fg = colors.purple_dark } -- Labels for goto - part of control flow
 
   -- TREESITTER HIGHLIGHTS
   -- Modern semantic highlighting preserving warm/cool principle
 
-  -- Warm tones for control and action
-  highlights["@keyword"] = { link = "Keyword" }
-  highlights["@keyword.conditional"] = { fg = colors.purple, bold = true }
-  highlights["@keyword.repeat"] = { fg = colors.purple, bold = true }
-  highlights["@keyword.return"] = { fg = colors.magenta, bold = true }
-  highlights["@keyword.function"] = { fg = colors.purple }
-  highlights["@keyword.operator"] = { fg = colors.purple_light }
-  highlights["@keyword.exception"] = { fg = colors.purple, bold = true }
+  -- Keywords - mostly de-emphasised
+  highlights["@keyword"] = { fg = colors.fg_dark }                 -- Most keywords are noise
+  highlights["@keyword.conditional"] = { fg = colors.purple_dark } -- if/else matter
+  highlights["@keyword.repeat"] = { fg = colors.purple_dark }      -- loops matter
+  highlights["@keyword.return"] = { fg = colors.purple }           -- return is important control flow
+  highlights["@keyword.function"] = { fg = colors.fg_dark }        -- 'function' keyword is noise
+  highlights["@keyword.operator"] = { fg = colors.fg }             -- and/or/not - blend in
+  highlights["@keyword.exception"] = { fg = colors.purple }        -- try/catch important
 
-  highlights["@function"] = { link = "Function" }
+  -- Functions - the main actors
+  highlights["@function"] = { fg = colors.magenta }
   highlights["@function.call"] = { fg = colors.magenta }
   highlights["@function.method"] = { fg = colors.magenta }
-  highlights["@function.method.call"] = { fg = colors.magenta }
-  highlights["@function.builtin"] = { fg = colors.magenta_light }
+  highlights["@function.method.call"] = { fg = colors.magenta_light }          -- Slightly different for method calls
+  highlights["@function.builtin"] = { fg = colors.magenta_light, bold = true } -- Built-ins are special
   highlights["@function.macro"] = { fg = colors.blue_light }
 
-  highlights["@variable"] = { link = "Variable" }
-  highlights["@variable.parameter"] = { fg = colors.orange_light }
+  -- Variables - warm but varied by scope/mutability
+  highlights["@variable"] = { fg = colors.orange }
+  highlights["@variable.parameter"] = { fg = colors.orange_light, italic = true } -- Parameters are special
   highlights["@variable.member"] = { fg = colors.orange }
-  highlights["@variable.builtin"] = { fg = colors.orange_dark }
+  highlights["@variable.builtin"] = { fg = colors.orange_dark, bold = true }      -- this/self/super
   highlights["@property"] = { fg = colors.orange }
   highlights["@field"] = { fg = colors.orange }
 
-  highlights["@constant"] = { link = "Constant" }
-  highlights["@constant.builtin"] = { fg = colors.orange_dark }
-  highlights["@constant.macro"] = { fg = colors.orange_dark }
-  highlights["@number"] = { link = "Number" }
-  highlights["@number.float"] = { link = "Float" }
-  highlights["@boolean"] = { link = "Boolean" }
+  -- Constants - clearly different from variables
+  highlights["@constant"] = { fg = colors.orange_dark, bold = true }
+  highlights["@constant.builtin"] = { fg = colors.orange_dark, bold = true }
+  highlights["@constant.macro"] = { fg = colors.orange_dark, bold = true }
+  highlights["@number"] = { fg = colors.orange_light }
+  highlights["@number.float"] = { fg = colors.orange_light }
+  highlights["@boolean"] = { fg = colors.orange_light }
 
-  -- Cool tones for data and structure
-  highlights["@string"] = { link = "String" }
-  highlights["@string.regex"] = { fg = colors.green_dark }
+  -- Strings - cool, calm data
+  highlights["@string"] = { fg = colors.green }
+  highlights["@string.regex"] = { fg = colors.green_dark, italic = true } -- Regex is special
   highlights["@string.escape"] = { fg = colors.green_light }
   highlights["@string.special"] = { fg = colors.green_light }
-  highlights["@character"] = { link = "Character" }
+  highlights["@character"] = { fg = colors.green }
   highlights["@character.special"] = { fg = colors.green_light }
 
-  highlights["@type"] = { link = "Type" }
+  -- Types - important structural information
+  highlights["@type"] = { fg = colors.cyan }
   highlights["@type.builtin"] = { fg = colors.cyan_light }
-  highlights["@type.definition"] = { fg = colors.cyan_dark }
-  highlights["@type.qualifier"] = { fg = colors.cyan }
+  highlights["@type.definition"] = { fg = colors.cyan_dark, bold = true } -- Type definitions are important
+  highlights["@type.qualifier"] = { fg = colors.fg_dark }                 -- const/mut qualifiers de-emphasised
   highlights["@constructor"] = { fg = colors.cyan_dark, bold = true }
   highlights["@module"] = { fg = colors.cyan }
   highlights["@namespace"] = { fg = colors.cyan }
 
-  highlights["@comment"] = { link = "Comment" }
-  highlights["@comment.documentation"] = { fg = colors.fg_dark, italic = true }
+  -- Comments - VISIBLE and useful
+  highlights["@comment"] = { fg = colors.fg_dark, italic = true }
+  highlights["@comment.documentation"] = { fg = colors.fg, italic = true } -- Doc comments even more visible
   highlights["@comment.error"] = { fg = colors.red, bold = true }
   highlights["@comment.warning"] = { fg = colors.yellow, bold = true }
   highlights["@comment.todo"] = { link = "Todo" }
   highlights["@comment.note"] = { link = "Note" }
 
-  highlights["@operator"] = { link = "Operator" }
-  highlights["@punctuation.delimiter"] = { link = "Delimiter" }
-  highlights["@punctuation.bracket"] = { fg = colors.fg_dark }
-  highlights["@punctuation.special"] = { fg = colors.blue }
+  -- Operators and punctuation - INVISIBLE
+  highlights["@operator"] = { fg = colors.fg }
+  highlights["@punctuation.delimiter"] = { fg = colors.fg }
+  highlights["@punctuation.bracket"] = { fg = colors.fg }
+  highlights["@punctuation.special"] = { fg = colors.fg_dark }
 
   -- Language-specific highlights
   highlights["@symbol.elixir"] = { fg = colors.green_dark }
-  highlights["@attribute.rust"] = { fg = colors.blue }
-  highlights["@label.rust"] = { fg = colors.blue }
+  highlights["@attribute.rust"] = { fg = colors.fg_dark } -- Attributes are mostly noise
+  highlights["@label.rust"] = { fg = colors.purple_dark }
 
   -- Markup (Markdown, etc.)
   highlights["@markup.heading"] = { fg = colors.purple, bold = true }
